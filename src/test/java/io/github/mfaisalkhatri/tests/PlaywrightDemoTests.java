306f5e59e861096f7c19b184a3c46eb47ada3ee7
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2024-03-08 21:02:12 +0530

    Added example code for browser navigations method in playwright (#58)
    * added example code for back,forward and reload options
    
    * added example code for back,forward and reload options
    
    * updated the code as per sonarlint smells and resolved them
    
    * added browser navigation test to testng.xml file

--- src/test/java/io/github/mfaisalkhatri/tests/PlaywrightDemoTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/PlaywrightDemoTests.java
@@ -89,4 +89,42 @@ public class PlaywrightDemoTests {
                 assertEquals(pageTitle, "Your Store");
                 browser.close();
         }
+
+    @Test
+    public void testBrowserNavigation() {
+        final Playwright playwright = Playwright.create();
+        final Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        final Page page = browser.newPage();
+        page.navigate("https://the-internet.herokuapp.com/");
+
+        final String homePageHeader = page.locator("h2").textContent();
+        assertEquals(homePageHeader, "Available Examples");
+
+        page.navigate("https://the-internet.herokuapp.com/challenging_dom");
+        final String challengingDomPageHeader = page.locator("h3").textContent();
+        assertEquals(challengingDomPageHeader, "Challenging DOM");
+
+        page.goBack();
+        // page.goBack(new Page.GoBackOptions().setWaitUntil(WaitUntilState.COMMIT));
+        // page.goBack(new Page.GoBackOptions().setTimeout(50));
+        assertEquals(homePageHeader, "Available Examples");
+
+        page.goForward();
+        // page.goForward(new Page.GoForwardOptions().setWaitUntil(WaitUntilState.LOAD));
+        //page.goForward(new Page.GoForwardOptions().setTimeout(40));
+        assertEquals(challengingDomPageHeader, "Challenging DOM");
+
+        page.reload();
+        // page.reload(new Page.ReloadOptions().setTimeout(60));
+        //page.reload(new Page.ReloadOptions().setWaitUntil(WaitUntilState.COMMIT));
+        assertEquals(challengingDomPageHeader, "Challenging DOM");
+
+        final String currentPageUrl = page.url();
+        final String websiteLink = "https://the-internet.herokuapp.com/challenging_dom";
+        assertEquals(currentPageUrl, websiteLink);
+
+        browser.close();
+
+    }
 }
+
