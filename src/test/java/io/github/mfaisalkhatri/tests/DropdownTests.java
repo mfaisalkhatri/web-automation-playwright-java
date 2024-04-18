38571354ac795c0aca33131e263f5ca1e5b93a53
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-04-18 17:03:51 +0530

    updated dropdown tests to gracefully close the browser and removed all extra spaces/lines from all test classes

--- src/test/java/io/github/mfaisalkhatri/tests/DropdownTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/DropdownTests.java
@@ -2,6 +2,7 @@ package io.github.mfaisalkhatri.tests;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.SelectOption;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -12,24 +13,24 @@ import static org.testng.Assert.assertEquals;
 
 public class DropdownTests {
 
+    private Playwright playwright;
     private Page page;
-    private Locator dropdownField;
+
 
     @BeforeClass
     public void setup() {
-        final Playwright playwright = Playwright.create();
+        this.playwright = Playwright.create();
         final Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chrome"));
-
         this.page = browser.newPage();
-        this.page.navigate("https://the-internet.herokuapp.com/dropdown");
-        this.dropdownField = this.page.locator("#dropdown");
-
     }
 
     @Test
     public void testSelectByOption() {
 
-        this.dropdownField.selectOption("Option 2");
+        this.page.navigate("https://the-internet.herokuapp.com/dropdown");
+        final Locator dropdownField = this.page.locator("#dropdown");
+
+        dropdownField.selectOption("Option 2");
 
         final String dropdownFieldSelectedValue = this.page.locator("#dropdown [selected='selected']").innerText();
         assertEquals(dropdownFieldSelectedValue, "Option 2");
@@ -38,7 +39,11 @@ public class DropdownTests {
 
     @Test
     public void testSelectByLabel() {
-        this.dropdownField.selectOption(new SelectOption().setLabel("Option 1"));
+
+        this.page.navigate("https://the-internet.herokuapp.com/dropdown");
+        final Locator dropdownField = this.page.locator("#dropdown");
+
+        dropdownField.selectOption(new SelectOption().setLabel("Option 1"));
 
         final String dropdownFieldSelectedValue = this.page.locator("#dropdown [selected='selected']").innerText();
         assertEquals(dropdownFieldSelectedValue, "Option 1");
@@ -47,31 +52,40 @@ public class DropdownTests {
 
     @Test
     public void testSelectByIndex() {
-        this.dropdownField.selectOption(new SelectOption().setIndex(2));
+
+        this.page.navigate("https://the-internet.herokuapp.com/dropdown");
+        final Locator dropdownField = this.page.locator("#dropdown");
+
+        dropdownField.selectOption(new SelectOption().setIndex(2));
 
         final String dropdownFieldSelectedValue = this.page.locator("#dropdown [selected='selected']").innerText();
         assertEquals(dropdownFieldSelectedValue, "Option 2");
 
     }
-
     @Test
     public void testSelectByValue() {
+
         this.page.navigate("https://www.lambdatest.com/selenium-playground/select-dropdown-demo");
+        final Locator dropdownField = this.page.locator("#select-demo");
 
-        this.dropdownField.selectOption(new SelectOption().setValue("Option 1"));
+        dropdownField.selectOption(new SelectOption().setValue("Wednesday"));
 
-        final String dropdownFieldSelectedValue = this.page.locator("#dropdown [selected='selected']").innerText();
-        assertEquals(dropdownFieldSelectedValue, "Option 1");
+        final Locator daySelected = this.page.locator(".pt-10 p");
+        assertThat(daySelected).hasText("Day selected :- Wednesday");
     }
 
     @Test
     public void testMultiSelectOptions() {
         this.page.navigate("https://www.lambdatest.com/selenium-playground/select-dropdown-demo");
 
-        this.dropdownField = this.page.locator("#multi-select");
-        this.dropdownField.selectOption(new SelectOption[]{new SelectOption().setLabel("New York"), new SelectOption().setLabel("Texas")});
+        final Locator dropdownField = this.page.locator("#multi-select");
+        dropdownField.selectOption(new SelectOption[]{new SelectOption().setLabel("New York"), new SelectOption().setLabel("Texas"), new SelectOption().setValue("California"), new SelectOption().setIndex(7)});
 
-        assertThat(this.dropdownField).hasValues(new Pattern[]{Pattern.compile("New York"), Pattern.compile("Texas")});
+        assertThat(dropdownField).hasValues(new Pattern[]{Pattern.compile("California"), Pattern.compile("New York"), Pattern.compile("Texas"), Pattern.compile("Washington")});
     }
 
+    @AfterClass
+    public void tearDown() {
+        this.playwright.close();
+    }
 }
