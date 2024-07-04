18ee6f35b84c2cc86521fb6326ddd5a1ef237ce9
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-07-04 11:54:43 +0530

    updated dependency versions in pom.xml and added a test for locating field by label

---
+++ src/test/java/io/github/mfaisalkhatri/tests/TextFieldTest.java
@@ -0,0 +1,26 @@
+package io.github.mfaisalkhatri.tests;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TextFieldTest {
+
+    private Playwright playwright;
+    private Page page;
+
+
+    @BeforeClass
+    public void setup() {
+        this.playwright = Playwright.create();
+        final Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chrome"));
+        this.page = browser.newPage();
+    }
+
+    @Test
+    public void locateFirstNameByLabel() {
+        page.navigate("https://practicesoftwaretesting.com/contact");
+        Locator firstNameField = page.getByLabel("First name");
+        firstNameField.fill("Faisal");
+    }
+}
