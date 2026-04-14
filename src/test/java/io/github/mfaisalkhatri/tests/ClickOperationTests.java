d63b54c49bd170bede1b03178200629782a3841f
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2026-04-14 16:49:10 +0530

    added example code for right mouse click

--- src/test/java/io/github/mfaisalkhatri/tests/ClickOperationTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/ClickOperationTests.java
@@ -4,9 +4,11 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.MouseButton;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -37,6 +39,18 @@ public class ClickOperationTests {
             this.page.getByRole (AriaRole.HEADING, new Page.GetByRoleOptions ().setName ("Challenging DOM"))).isVisible ();
     }
 
+    @Test
+    public void testRightClick () {
+        this.page.navigate ("https://the-internet.herokuapp.com/context_menu");
+        this.page.locator ("#hot-spot")
+            .click (new Locator.ClickOptions ().setButton (MouseButton.RIGHT));
+        this.page.onDialog (dialog -> {
+            String alertText = dialog.message ();
+
+            assert alertText.equals ("You selected a context menu");
+            dialog.accept ();
+        });
+    }
 
     @AfterClass
     public void tearDown() {
