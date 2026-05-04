f4f0100ad1db72903605ec7715aaa3db477a7d62
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-05-04 08:00:32 +0530

    updated playwright dependency version, added test for delay click, drag and drop and click with modifiers

--- src/test/java/io/github/mfaisalkhatri/tests/ClickOperationTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/ClickOperationTests.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
@@ -17,110 +18,138 @@ import static org.testng.Assert.assertEquals;
 public class ClickOperationTests {
 
     private Playwright playwright;
-    private Page page;
+    private Page       page;
 
     @BeforeMethod
-    public void setup() {
-        this.playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)
-                .setChannel("chrome"));
-        this.page = browser.newPage();
+    public void setup () {
+        this.playwright = Playwright.create ();
+        Browser browser = this.playwright.chromium ()
+            .launch (new BrowserType.LaunchOptions ().setHeadless (false)
+                .setChannel ("chrome"));
+        this.page = browser.newPage ();
     }
 
     @Test
-    public void testLeftClick() {
-        this.page.navigate("https://the-internet.herokuapp.com/");
-        this.page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Challenging DOM"))
-                .click();
+    public void testLeftClick () {
+        this.page.navigate ("https://the-internet.herokuapp.com/");
+        this.page.getByRole (AriaRole.LINK, new Page.GetByRoleOptions ().setName ("Challenging DOM"))
+            .click ();
         //Locator challenginDomLink = page.getByRole (AriaRole.LINK,
         //  new Page.GetByRoleOptions ().setName ("Challenging DOM"));
         //challenginDomLink.click ();
 
-        assertThat(
-                this.page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Challenging DOM"))).isVisible();
+        assertThat (this.page.getByRole (AriaRole.HEADING,
+            new Page.GetByRoleOptions ().setName ("Challenging DOM"))).isVisible ();
     }
 
     @Test
-    public void testRightClick() {
-        this.page.navigate("https://the-internet.herokuapp.com/context_menu");
-        this.page.locator("#hot-spot")
-                .click(new Locator.ClickOptions().setButton(MouseButton.RIGHT));
-        this.page.onDialog(dialog -> {
-            String alertText = dialog.message();
-            assert alertText.equals("You selected a context menu");
-            dialog.accept();
+    public void testRightClick () {
+        this.page.navigate ("https://the-internet.herokuapp.com/context_menu");
+        this.page.locator ("#hot-spot")
+            .click (new Locator.ClickOptions ().setButton (MouseButton.RIGHT));
+        this.page.onDialog (dialog -> {
+            String alertText = dialog.message ();
+            assert alertText.equals ("You selected a context menu");
+            dialog.accept ();
         });
     }
 
     @Test
-    public void testDoubleClick() {
-        this.page.navigate("https://demo.guru99.com/test/simple_context_menu.html");
+    public void testDoubleClick () {
+        this.page.navigate ("https://demo.guru99.com/test/simple_context_menu.html");
 
-        Locator alertBtn = this.page.locator("#authentication > button");
-        this.page.onDialog(dialog -> {
+        Locator alertBtn = this.page.locator ("#authentication > button");
+        this.page.onDialog (dialog -> {
             String text = "You double clicked me.. Thank You..";
-            assertEquals(dialog.message(), text);
-            dialog.accept();
+            assertEquals (dialog.message (), text);
+            dialog.accept ();
         });
-        alertBtn.dblclick();
+        alertBtn.dblclick ();
     }
 
     @Test
-    public void testMouseHover() {
-        this.page.navigate("https://automationteststore.com/");
-        this.page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions()
-                .setName("  Apparel & accessories")).hover();
-        this.page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions()
-                .setName("Shoes").setExact(true)).click();
-        assertThat(this.page.getByRole(AriaRole.HEADING,
-                new Page.GetByRoleOptions().setName("Shoes"))).isVisible();
+    public void testMouseHover () {
+        this.page.navigate ("https://automationteststore.com/");
+        this.page.getByRole (AriaRole.LINK, new Page.GetByRoleOptions ().setName ("  Apparel & accessories"))
+            .hover ();
+        this.page.getByRole (AriaRole.LINK, new Page.GetByRoleOptions ().setName ("Shoes")
+                .setExact (true))
+            .click ();
+        assertThat (
+            this.page.getByRole (AriaRole.HEADING, new Page.GetByRoleOptions ().setName ("Shoes"))).isVisible ();
     }
 
     @Test
-    public void testForceMouseClick() {
-        this.page.navigate("https://automationteststore.com/");
-        this.page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions()
-                .setName("Specials")).click(new Locator.ClickOptions().setForce(true));
-        assertThat(this.page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions()
-                .setName("Special Offers"))).isVisible();
+    public void testForceMouseClick () {
+        this.page.navigate ("https://automationteststore.com/");
+        this.page.getByRole (AriaRole.LINK, new Page.GetByRoleOptions ().setName ("Specials"))
+            .click (new Locator.ClickOptions ().setForce (true));
+        assertThat (this.page.getByRole (AriaRole.HEADING,
+            new Page.GetByRoleOptions ().setName ("Special Offers"))).isVisible ();
     }
 
     @Test
-    public void testPositionBasedClick() {
-        this.page.navigate("https://the-internet.herokuapp.com/horizontal_slider");
-        Locator slider = this.page.getByRole(AriaRole.SLIDER);
-        BoundingBox box = slider.boundingBox();
+    public void testPositionBasedClick () {
+        this.page.navigate ("https://the-internet.herokuapp.com/horizontal_slider");
+        Locator slider = this.page.getByRole (AriaRole.SLIDER);
+        BoundingBox box = slider.boundingBox ();
         double width = box.width;
         double height = box.height;
 
         double x = width * 0.5;
         double y = height * 0.5;
 
-        slider.click(new Locator.ClickOptions().setPosition(x, y));
-        this.page.waitForTimeout(3000);
+        slider.click (new Locator.ClickOptions ().setPosition (x, y));
+        this.page.waitForTimeout (3000);
 
     }
 
     @Test
-    public void testClickWithModifiers() {
-        page.navigate("https://jqueryui.com/selectable/");
-        FrameLocator frame = page.frameLocator(".demo-frame");
+    public void testClickWithModifiers () {
+        this.page.navigate ("https://jqueryui.com/selectable/");
+        FrameLocator frame = this.page.frameLocator (".demo-frame");
 
-        Locator items = frame.locator("#selectable li");
+        Locator items = frame.locator ("#selectable li");
 
-        items.nth(0).click(new Locator.ClickOptions()
-                .setModifiers(List.of(KeyboardModifier.CONTROL)));
+        items.nth (0)
+            .click (new Locator.ClickOptions ().setModifiers (List.of (KeyboardModifier.CONTROL)));
 
-        items.nth(3).click(new Locator.ClickOptions()
-                .setModifiers(List.of(KeyboardModifier.CONTROL)));
+        items.nth (3)
+            .click (new Locator.ClickOptions ().setModifiers (List.of (KeyboardModifier.CONTROL)));
 
-        page.waitForTimeout(4000);
+        this.page.waitForTimeout (2000);
+
+        this.page.navigate ("https://the-internet.herokuapp.com/");
+        this.page.getByRole (AriaRole.LINK, new Page.GetByRoleOptions ().setName ("File Download")
+                .setExact (true))
+            .click (new Locator.ClickOptions ().setModifiers (List.of (KeyboardModifier.SHIFT)));
+
+        this.page.waitForTimeout (2000);
+
+    }
+
+    @Test
+    public void testDelayClick () {
+        this.page.navigate ("https://the-internet.herokuapp.com/");
+        this.page.getByRole (AriaRole.LINK, new Page.GetByRoleOptions ().setName ("Challenging DOM"))
+            .click (new Locator.ClickOptions ().setDelay (2000));
+        assertThat (this.page.getByRole (AriaRole.HEADING,
+            new Page.GetByRoleOptions ().setName ("Challenging DOM"))).isVisible ();
+    }
+
+    @Test
+    public void testDragAndDropActions () {
+        this.page.navigate ("https://the-internet.herokuapp.com/");
+        this.page.getByRole (AriaRole.LINK, new Page.GetByRoleOptions ().setName ("Drag and Drop"))
+            .click ();
+        this.page.locator ("#column-a").dragTo (this.page.locator ("#column-b"));
+        assertThat(this.page.locator ("#column-a")).containsText ("B");
 
     }
 
     @AfterMethod
-    public void tearDown() {
-        this.page.close();
-        this.playwright.close();
+    public void tearDown () {
+        this.page.close ();
+        this.playwright.close ();
     }
 }
